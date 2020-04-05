5b7ca44b61eb1f80c3c01dd6f825cc0dc1a25854
Wilson Godoi <wilsonggodoi@gmail.com>
2020-04-05 16:00:48 -0300

    config db into cloud

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   privateKey: '2f639018-7c65-44df-a3a2-a070bd2a10a8',
-  connectionUrlDatabase: 'mongodb://127.0.0.1:27017/car-sales',
+  connectionUrlDatabase:
+    'mongodb+srv://wilsongodoi:wilsongodoi@cluster0-aqmca.mongodb.net/test?retryWrites=true&w=majority',
   apiPort: 8080,
   corsOptions: {
     origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
