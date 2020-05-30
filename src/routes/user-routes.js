c3d064f5940345f71f1664619d5d6850e97b77c6
Wilson Godoi <wilsonggodoi@gmail.com>
2020-05-30 17:28:32 -0300

    user get current docs is done

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -39,20 +39,43 @@ const routes = Router();
  */
 routes.get('/', authService.isAdmin, UserController.list);
 
-routes.get('/current', authService.authorize, UserController.getCurrent);
-
 routes.post('/', authService.isAdmin, UserController.create);
 
-routes.post('/avatar', authService.authorize, UserController.saveAvatar);
-
 routes.put('/:id', authService.isAdmin, UserController.edit);
+
+routes.delete('/:id', authService.isAdmin, UserController.delete);
+
+/**
+ * @swagger
+ * /api/auth/users/current:
+ *  get:
+ *      security:
+ *          - bearerAuth: []
+ *      summary: Use to get a current user.
+ *      tags:
+ *          - name: Users
+ *      responses:
+ *          '200':
+ *              description: A succesful response
+ *              examples:
+ *                    application/json:
+ *                        {
+ *                          id: string,
+ *                          login: string,
+ *                          name: string,
+ *                          type: string,
+ *                          active: boolean,
+ *                          imageBase64: string
+ *                        }
+ */
+routes.get('/current', authService.authorize, UserController.getCurrent);
+
+routes.post('/avatar', authService.authorize, UserController.saveAvatar);
 
 routes.put(
   '/current/password',
   authService.authorize,
   UserController.changePassword
 );
-
-routes.delete('/:id', authService.isAdmin, UserController.delete);
 
 module.exports = app => app.use('/api/auth/users', routes);
