8cccd9c442f8676f38bda6840047a53d2c20265d
Wilson Godoi <wilsonggodoi@gmail.com>
2020-05-30 16:12:47 -0300

    logout docs is done

--- src/routes/logout-routes.js
+++ src/routes/logout-routes.js
@@ -4,6 +4,21 @@ const authService = require('../services/auth-service');
 
 const routes = Router();
 
+/**
+ * @swagger
+ * /api/auth/logout:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Use to do a logout.
+ *     tags:
+ *       - name: Logout
+ *     consumes:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Logout
+ */
 routes.post('/', authService.authorize, LogoutController.doLogout);
 
-module.exports = app => app.use('/api/auth/users/logout', routes);
+module.exports = app => app.use('/api/auth/logout', routes);
