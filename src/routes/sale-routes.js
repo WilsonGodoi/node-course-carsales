39d2ed4991c41db1ff1a7f8322ea3fd3ee2fb523
Wilson Godoi <wilsonggodoi@gmail.com>
2020-06-01 22:15:37 -0300

    sales docs is done

--- src/routes/sale-routes.js
+++ src/routes/sale-routes.js
@@ -20,6 +20,7 @@ const routes = Router();
  *                    application/json:
  *                      [
  *                        {
+ *                          _id: string,
  *                          customer: { _id: string, name: string, email: string, telephone: string },
  *                          seller: { _id: string, login: string, name: string, type: string, active: boolean },
  *                          vehicle: { _id: string, brand: string, model: string, modelYear: string, manufactureYear: string, color: string, mileage: string, onlyOwner: boolean, status: string, price: number },
@@ -30,6 +31,32 @@ const routes = Router();
  */
 routes.get('/', authService.authorize, SaleController.list);
 
+/**
+ * @swagger
+ * /api/auth/sales:
+ *  post:
+ *      security:
+ *          - bearerAuth: []
+ *      summary: Use to create a sale.
+ *      tags:
+ *          - name: Sales
+ *      parameters:
+ *      - in: body
+ *        name: user
+ *        description: The sale to create.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            customerId:
+ *              type: string
+ *            vehicleId:
+ *              type: string
+ *            value:
+ *              type: number
+ *      responses:
+ *          '201':
+ *              description: A succesful response
+ */
 routes.post('/', authService.authorize, SaleController.create);
 
 module.exports = app => app.use('/api/auth/sales', routes);
