3043e5cda01861d69c09cbf1541f553cf1e45090
Luis Henrique <selesluishenrique@gmail.com>
2022-06-16 16:14:11 -0300

    Configurações iniciais e conexão com o banco de dados Mondo DB

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   privateKey: '2f639018-7c65-44df-a3a2-a070bd2a10a8',
   // connectionUrlDatabase: 'mongodb://127.0.0.1:27017/car-sales',
-  connectionUrlDatabase: '',
+  connectionUrlDatabase: 'mongodb+srv://LuisHenriqueSeles:lDHzamApGNoKLyV8@carsaledb.gkvms8m.mongodb.net/?retryWrites=true&w=majority',
   apiPort: 8080,
   corsOptions: {
     origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
