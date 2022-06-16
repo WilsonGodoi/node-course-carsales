1767aa3d49459cf8f85ab96f21ea30890c4fcc87
Luis Henrique <selesluishenrique@gmail.com>
2022-06-16 16:55:28 -0300

    Configurações iniciais do projeto e conexão com o banco de dados mongo db

---
+++ angular-ecommerce-carsales/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Bem vindo(a) ao E-commerce CarSales';
+}
